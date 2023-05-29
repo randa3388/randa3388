2788d52a3e950b87cf73473d82e8f7caaa3f4d5e
randa3388 <134943815+randa3388@users.noreply.github.com>
2023-05-29 16:32:03 +0300

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace test2_sql_f
+{
+    public partial class App : Application
+    {
+        static UserOperation db;
+
+        // Create the database connection as a singleton.
+        public static UserOperation UserSQLite
+        {
+            get
+            {
+                if (db == null)
+                {
+                    db = new UserOperation(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "NumberCodeOpreration.db3"));
+                }
+                return db;
+            }
+        }
+        Users user;
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage( new HomePage(user));
+
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}

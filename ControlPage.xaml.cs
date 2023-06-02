86d8a235bd9f80d6c8248ca6e1d45a17f8105bad
randa3388 <134943815+randa3388@users.noreply.github.com>
2023-06-02 22:08:36 +0300

    Update ControlPage.xaml.cs

--- ControlPage.xaml.cs
+++ ControlPage.xaml.cs
@@ -10,17 +10,23 @@ using Xamarin.Forms.Xaml;
 
 namespace test2_sql_f
 {
-    [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class ControlPage : ContentPage
-    {
-        public Users user;
+    public Users user;
 
         public ControlPage(Users users)
         {
             InitializeComponent();
             this.user = (Users)users;
+           number.Text = Convert.ToString(user.Number);
+           code.Text = Convert.ToString(user.Code);
 
         }
+        public ControlPage()
+        {
+            InitializeComponent();
+           
+
+        }
+
 
         private async void create_Clicked(object sender, EventArgs e)
         {
@@ -28,7 +34,7 @@ namespace test2_sql_f
             {
                 var user = new Users()
                 {
-                    Number =Convert.ToInt32( number.Text),
+                    Number = Convert.ToInt32(number.Text),
                     Code = Convert.ToInt32(code.Text),
 
                 };
@@ -42,20 +48,33 @@ namespace test2_sql_f
 
         private async void update_Clicked(object sender, EventArgs e)
         {
-            this.user.Number = Convert.ToInt32( number.Text);
-            this.user.Code = Convert.ToInt32(code.Text);
+            if ((!string.IsNullOrEmpty(number.Text)) && (!string.IsNullOrEmpty(code.Text)))
+            {
+                this.user.Number = Convert.ToInt32(number.Text);
+                this.user.Code = Convert.ToInt32(code.Text);
 
 
 
-            await App.UserSQLite.SaveUserAsync(this.user);
-            await Navigation.PushAsync(new LoginPage());
+                await App.UserSQLite.SaveUserAsync(this.user);
+                await Navigation.PushAsync(new LoginPage());
+            }else
+                await DisplayAlert("Error", "plese sing in", "Ok");
+
+
+
         }
 
         private async void delete_Clicked(object sender, EventArgs e)
         {
+            if ((!string.IsNullOrEmpty(number.Text)) && (!string.IsNullOrEmpty(code.Text)))
+            {
+                await App.UserSQLite.DeleteUserAsync(this.user);
+                await Navigation.PopToRootAsync();
+            }
+            else
+                await DisplayAlert("Error", "plese sing in", "Ok");
 
-            await App.UserSQLite.DeleteUserAsync(this.user);
-            await Navigation.PopToRootAsync();
+
 
         }
 
